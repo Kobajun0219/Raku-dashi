1efeeb7b2ac5ba7c3ec6f10cae1f3614665ff7d1
EC2 Default User <ec2-user@ip-172-31-13-127.ap-northeast-1.compute.internal>
2021-09-24 05:43:31 +0000

    fix com/blade

--- resources/views/common/com.blade.php
+++ resources/views/common/com.blade.php
@@ -1,13 +1,15 @@
 <!-- resources/views/common/errors.blade.php -->
-@if (count($com) > 0)
-    <!-- Form Error List -->
-    <div class="alert alert alert-warning" id="com">
-        <div><strong>{{$com[0]}}完了しました！</strong></div> 
-    </div>
-    
-    <script>
-          setTimeout(() => {
-            $('#com').fadeOut();
-          }, 2000);
-    </script>
-@endif
+@if (isset($com))
+    @if (count($com) > 0)
+        <!-- Form Error List -->
+        <div class="alert alert alert-warning" id="com">
+            <div><strong>{{$com[0]}}完了しました！</strong></div> 
+        </div>
+        
+        <script>
+              setTimeout(() => {
+                $('#com').fadeOut();
+              }, 2000);
+        </script>
+    @endif
+ @endif
