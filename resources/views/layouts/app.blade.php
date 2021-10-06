fe97d4ffb877ff38301a2d7390e9cb89064cc05d
EC2 Default User <ec2-user@ip-172-31-13-127.ap-northeast-1.compute.internal>
2021-10-06 17:49:43 +0000

    change font and delete app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,13 @@
     <link rel="icon" href="{{url('image/chara.png')}}">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300&display=swap" rel="stylesheet">
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
